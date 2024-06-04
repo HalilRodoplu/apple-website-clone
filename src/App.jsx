8ead2183dd5a13f9d4c1db0df7816af6e85cf672
Halil Rodoplu <halil.rodoplu@mepsan.com.tr>
2024-06-04 17:04:13 +0300

    videocarousel component created and improving to animations with gsap

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Hero from "./components/Hero.jsx";
+
+const App = () => {
+
+  return (
+      <main className="bg-black">
+          <Navbar/>
+          <Hero/>
+          <Highlights/>
+      </main>
+  )
+}
+
+export default App
