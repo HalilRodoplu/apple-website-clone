ddfc1f78b57564aa46a95ad599b8601d43b07e72
Halil Rodoplu <halil.rodoplu@mepsan.com.tr>
2024-06-05 13:31:03 +0300

    model section and the other components are created. iPhone model can be seen as 3d model

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -131,6 +131,10 @@ const VideoCarousel = () => {
                 setVideo((pre) => ({...pre, isPlaying: !pre.isPlaying}))
                 break;
 
+            case "pause":
+                setVideo((pre) => ({...pre, isPlaying: !pre.isPlaying}))
+                break;
+
             default:
                 return video;
         }
@@ -149,6 +153,7 @@ const VideoCarousel = () => {
                                     playsInline={true}
                                     preload="auto"
                                     muted
+                                    className={`${list.id === 2 && "translate-x-44"} pointer-events-none`}
                                     ref={(el) => (videoRef.current[i] = el)}
                                     onEnded={() => i !== 3 ? handleProcess("video-end", i) : handleProcess("video-last")}
                                     onPlay={()=> {setVideo((prevVideo) => ({...prevVideo, isPlaying: true}))}}
